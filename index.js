d7aad24eae5f8f163b47fdc236c467166933867e
Abu Al Maruf <mdmaruf55667711@gmail.com>
2024-05-31 00:46:06 +0600

    post data in mongodb and get on server

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrrlkes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const brandCollection = client.db("brandDB").collection("brands");
+
+    app.post("/brandProducts", async (req, res) => {
+      console.log("post hitted");
+      const brand = req.body;
+      const result = await brandCollection.insertOne(brand);
+      res.send(result);
+    });
+    app.get("/brandProducts", async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server Running....");
